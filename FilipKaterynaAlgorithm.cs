7a6793ff58f68ca3db00d24399f0e50418c99974
Katya Philip <philipkatya13@gmail.com>
2024-09-25 02:08:53 +0300

    Add project files.

---
+++ FilipKaterynaAlgorithm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Robot.Common;
+
+namespace FilipKateryna.RobotChallange
+{
+    public class FilipKaterynaAlgorithm : IRobotAlgorithm
+    {
+        private const int maxRobots = 100;
+        private int robotCount = 10;
+        public string Author => "Filip Kateryna";
+
+        public int Round { get; private set; }
+
+        public FilipKaterynaAlgorithm()
+        {
+            Logger.OnLogRound += Logger_OnLogRound;
+        }
+
+        private void Logger_OnLogRound(object sender, LogRoundEventArgs e)
+        {
+            Round++;
+        }
+
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            var myRobots = Functions.GetMyRobots(robots);
+            var movingRobot = robots[robotToMoveIndex];
+
+            // 1. If there is enough energy and my robots count < 100, create new robot
+            if (movingRobot.Energy >= 300 && robotCount < maxRobots)
+            {
+                robotCount++;
+                return new CreateNewRobotCommand() { NewRobotEnergy = 50 };
+            }
+
+            // 2. Calculate profit of 4 possible moves:
+
+            // 2.1) Collect energy on the current station
+            var availableEnergy = Functions.EnergyToBeCollected(map, movingRobot.Position);
+
+            // 2.2) Attack another robot
+            var bestRobotToAttack = Functions.FindBestRobotToAttack(robots, movingRobot);
+            var profitFromAttacking = bestRobotToAttack.Item2;
+
+            // 2.3) Go to the nearest station
+            var nearestStation = Functions.FindNearestFreeStation(movingRobot, map, robots);
+            var profitFromNearestStation = nearestStation != null
+                ? Functions.ProfitFromStationMove(movingRobot, nearestStation.Position, nearestStation.Energy)
+                : 0;
+
+            // 2.4) Go to the best station
+            var bestStation = Functions.FindBestFreeStation(movingRobot, map, robots);
+            var profitFromBestStation = bestStation != null
+                ? Functions.ProfitFromStationMove(movingRobot, bestStation.Position, bestStation.Energy)
+                : 0;
+
+            // 3. Choose the move with the best profit
+            var bestProfit = Math.Max(Math.Max(availableEnergy, profitFromAttacking), Math.Max(profitFromNearestStation, profitFromBestStation));
+
+            // 4. If the profit is good, make the move
+            if (bestProfit > 0)
+            {
+                if (bestProfit == availableEnergy)
+                {
+                    return new CollectEnergyCommand();
+                }
+                if (bestProfit == profitFromAttacking)
+                {
+                    return new MoveCommand() { NewPosition = bestRobotToAttack.Item1.Position };
+                }
+                if (bestProfit == profitFromNearestStation)
+                {
+                    Position nearestCollectablePosition = Functions.FindNearestCollectablePosition(movingRobot.Position, nearestStation.Position, 2);
+                    return new MoveCommand() { NewPosition = nearestCollectablePosition };
+                }
+                if (bestProfit == profitFromBestStation)
+                {
+                    Position nearestCollectablePosition = Functions.FindNearestCollectablePosition(movingRobot.Position, bestStation.Position, 2);
+                    return new MoveCommand() { NewPosition = nearestCollectablePosition };
+                }
+            }
+
+            // 5. If profit is low, make 1 step further to the nearest/best station
+            var stepCloserPosition = nearestStation != null
+                ? Functions.GetOneStepCloser(movingRobot.Position, nearestStation.Position)
+                : Functions.GetOneStepCloser(movingRobot.Position, bestStation.Position);
+
+            return new MoveCommand() { NewPosition = stepCloserPosition };
+        }
+    }
+}
---
+++ Functions.cs
@@ -0,0 +1,95 @@
+﻿using Robot.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilipKateryna.RobotChallange
+{
+    public static class Functions
+    {
+        public static int EnergyToMove(Position a, Position b) =>
+            (int)(Math.Pow(b.X - a.X, 2.0) + Math.Pow(b.Y - a.Y, 2.0));
+
+        public static int Distance(Position pos1, Position pos2) => 
+            Math.Abs(pos1.X - pos2.X) + Math.Abs(pos1.Y - pos2.Y);
+
+
+        public static int EnergyToBeCollected(Map map, Position robotPos) =>
+            map.Stations.Where(station =>
+                Math.Abs(station.Position.X - robotPos.X) <= 2 &&
+                Math.Abs(station.Position.Y - robotPos.Y) <= 2)
+                .Sum(station => station.Energy);
+
+        public static bool StationIsFree(EnergyStation station, Robot.Common.Robot movingRobot, IList<Robot.Common.Robot> robots)
+        {
+            return Enumerable.Range(station.Position.X - 2, 5)
+                .SelectMany(x => Enumerable.Range(station.Position.Y - 2, 5), (x, y) => new Position(x, y))
+                .All(pos => CellIsFree(pos, movingRobot, robots));
+        }
+
+        public static bool CellIsFree(Position cell, Robot.Common.Robot movingRobot, IList<Robot.Common.Robot> robots)
+        {
+            return robots.Where(robot => robot != movingRobot)
+                         .All(robot => robot.Position != cell);
+        }
+
+        public static EnergyStation FindNearestFreeStation(Robot.Common.Robot movingRobot, Map map, IList<Robot.Common.Robot> robots)
+        {
+            return map.Stations
+                .Where(station => StationIsFree(station, movingRobot, robots) && Distance(movingRobot.Position, station.Position) > 2)
+                .OrderBy(station => EnergyToMove(station.Position, movingRobot.Position))
+                .FirstOrDefault();
+        }
+
+        public static EnergyStation FindBestFreeStation(Robot.Common.Robot movingRobot, Map map, IList<Robot.Common.Robot> robots)
+        {
+            return map.Stations
+                .Where(station => StationIsFree(station, movingRobot, robots) && Distance(movingRobot.Position, station.Position) > 2)
+                .OrderByDescending(station => station.Energy)
+                .ThenBy(station => EnergyToMove(station.Position, movingRobot.Position))
+                .FirstOrDefault();
+        }
+
+        public static Tuple<Robot.Common.Robot, int> FindBestRobotToAttack(IList<Robot.Common.Robot> robots, Robot.Common.Robot currentRobot)
+        {
+            var bestAttack = robots
+                .Where(robot => robot != currentRobot && !robot.OwnerName.Equals(currentRobot.OwnerName))
+                .Select(robot => new
+                {
+                    Robot = robot,
+                    MoveCost = EnergyToMove(currentRobot.Position, robot.Position),
+                    Profit = (int)(robot.Energy * 0.1 - 30 - EnergyToMove(currentRobot.Position, robot.Position))
+                })
+                .Where(r => r.MoveCost <= r.Robot.Energy && r.Profit > 0)
+                .OrderByDescending(r => r.Profit)
+                .Select(r => Tuple.Create(r.Robot, r.Profit))
+                .FirstOrDefault();
+
+            return bestAttack ?? new Tuple<Robot.Common.Robot, int>(null, 0);
+        }
+
+        public static List<Robot.Common.Robot> GetMyRobots(IList<Robot.Common.Robot> allRobots) =>
+            allRobots.Where(robot => robot.OwnerName.Equals("Filip Kateryna")).ToList();
+
+        public static Position GetOneStepCloser(Position current, Position target)
+        {
+            int newX = current.X + (current.X < target.X ? 1 : current.X > target.X ? -1 : 0);
+            int newY = current.Y + (current.Y < target.Y ? 1 : current.Y > target.Y ? -1 : 0);
+            return new Position(newX, newY);
+        }
+
+        public static int ProfitFromStationMove(Robot.Common.Robot movingRobot, Position stationPosition, int stationEnergy)
+        {
+            var nearestCollectablePosition = FindNearestCollectablePosition(movingRobot.Position, stationPosition, 2);
+            return stationEnergy - EnergyToMove(movingRobot.Position, nearestCollectablePosition);
+        }
+
+        public static Position FindNearestCollectablePosition(Position robotPosition, Position stationPosition, int radius)
+        {
+            return Enumerable.Range(stationPosition.X - radius, 2 * radius + 1)
+                .SelectMany(x => Enumerable.Range(stationPosition.Y - radius, 2 * radius + 1), (x, y) => new Position(x, y))
+                .OrderBy(pos => EnergyToMove(robotPosition, pos))
+                .FirstOrDefault();
+        }
+    }
+}
